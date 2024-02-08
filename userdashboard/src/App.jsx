073fc105358e34868ed67f5e47756952a2295378
olamide folayan <84618342+holladredd@users.noreply.github.com>
2024-02-08 15:54:09 +0000

    setting router

--- userdashboard/src/App.jsx
+++ userdashboard/src/App.jsx
@@ -3,26 +3,15 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Box, Grid } from "@mui/material";
-import Sidebar from './components/Sidebar'
-import Leftbar from './components/Leftbar'
+import { Box } from '@mui/material';
+import Dashboard from './pages/Dashboard';
 function App() {
 
 
   return (
-   <Box sx={{padding:'5px', backgroundColor:'whitesmoke'}}>
-
-  <Grid container spacing={2} sx={{ height:'100%', justifyContent:'space-between'}} >
-<Grid item xs={2} >
-  <Leftbar/>
-</Grid>
-<Grid item xs={10}>
-<Sidebar/>
-</Grid>
-
-  </Grid>
-
-   </Box>
+<Box>
+<Dashboard/>
+</Box>
   )
 }
 
---
+++ userdashboard/src/pages/Dashboard.jsx
@@ -0,0 +1,21 @@
+import { Box, Grid } from "@mui/material";
+import Sidebar from '../components/Sidebar'
+import Leftbar from '../components/Leftbar'
+const Dashboard = () => {
+    return (
+        <Box sx={{padding:'5px', backgroundColor:'whitesmoke'}}>
+
+        <Grid container spacing={2} sx={{ height:'100%', justifyContent:'space-between'}} >
+      <Grid item xs={2} >
+        <Leftbar/>
+      </Grid>
+      <Grid item xs={10}>
+      <Sidebar/>
+      </Grid>
+      
+        </Grid>
+      
+         </Box>
+    )
+}
+export default Dashboard
